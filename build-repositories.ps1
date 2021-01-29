28dbed1b894185008f6a87eb9fdac7e3d52b3b95
Paul Adams <padams@sdl.com>
2021-01-29 17:08:16 +0000

    Generates final zip of artifacts

--- build-repositories.ps1
+++ build-repositories.ps1
@@ -16,7 +16,13 @@ param (
    [bool]$update = $false,
 
    # Version to tag with
-   [string]$version = "0.0.0.0"
+   [string]$version = "0.0.0.0",
+
+   # True if we should first clone the repositories (they may already be cloned from a previous run)
+   [bool]$clone = $true,
+
+   # True if we should build the repositories (they may already be built from a previous run)
+   [bool]$build = $true
 )
 
 
@@ -67,21 +73,27 @@ function BuildDotnet($buildFileLocation, $generateArtifacts) {
 }
 
 # Clone all dotnet repositories required
-CloneRepo "dxa-web-application-dotnet"
-CloneRepo "dxa-modules"
-CloneRepo "dxa-content-management"
-CloneRepo "graphql-client-dotnet"
+if($clone) {
+   CloneRepo "dxa-web-application-dotnet"
+   CloneRepo "dxa-modules"
+   CloneRepo "dxa-content-management"
+   CloneRepo "graphql-client-dotnet"
+}
 
 # Build each dotnet repository and generate artifacts
-BuildDotnet "./repositories/dxa-web-application-dotnet/ciBuild.proj" $true
-BuildDotnet "./repositories/dxa-content-management/ciBuild.proj" $true
-BuildDotnet "./repositories/dxa-modules/webapp-net/ciBuild.proj" $true
-BuildDotnet "./repositories/graphql-client-dotnet/net/Build.csproj" $false
+if($build) {
+   BuildDotnet "./repositories/dxa-web-application-dotnet/ciBuild.proj" $true
+   BuildDotnet "./repositories/dxa-content-management/ciBuild.proj" $true
+   BuildDotnet "./repositories/dxa-modules/webapp-net/ciBuild.proj" $true
+   BuildDotnet "./repositories/graphql-client-dotnet/net/Build.csproj" $false
+}
 
 # Copy artifacts out to /artifacts folder
 # * all nuget packages
 # * all artifacts generated from each repository
 # * build zip(s)
+Write-Output "Packaging up artifacts ..."
+Write-Output "  \artifacts\dotnet will contain all DXA dotnet artifacts ..."
 $packageVersion = $version.Substring(0, $version.LastIndexOf("."))
 
 # Copy nuget packages from repositories
@@ -105,3 +117,9 @@ New-Item -ItemType Directory -Force -Path "artifacts/dotnet/cms" | Out-Null
 New-Item -ItemType Directory -Force -Path "artifacts/dotnet/ImportExport" | Out-Null
 Copy-Item -Path "./repositories/dxa-content-management/dist/cms/*" -Destination "artifacts/dotnet/cms" -Recurse -Force
 Copy-Item -Path "./repositories/dxa-content-management/dist/ImportExport/*" -Destination "artifacts/dotnet/ImportExport" -Recurse -Force
+
+
+$exclude = @("nuget")
+$files = Get-ChildItem -Path "artifacts/dotnet" -Exclude $exclude
+$collapsedVersion = $packageVersion -replace '[.]',''
+Compress-Archive -Path $files -DestinationPath "artifacts/dotnet/CM_DXA$collapsedVersion.zip" -CompressionLevel Fastest -Update
